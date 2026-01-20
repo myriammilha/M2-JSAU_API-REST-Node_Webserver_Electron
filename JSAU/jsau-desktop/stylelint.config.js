570ea05ae2d69f8150aa97d53b52b99bcba42bd2
Myriam Milha <myriam.milha@edu.univ-paris13.fr>
2026-01-20 01:14:30 +0100

    Add Stylelint configuration for desktop app

---
+++ JSAU/jsau-desktop/stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: 'stylelint-config-standard',
+    rules: {
+        'color-hex-length': 'short',
+        'no-empty-source': null,
+    },
+    ignoreFiles: ['node_modules/**/*.css', 'dist/**/*.css', 'coverage/**/*.css'],
+
+};
